62b9e478e5113eb147be83d959471c6491d91feb
Nathan Letwory <nathan@mcneel.com>
2019-03-07 12:20:37 +0200

    Opeta PGN-lukijaa tiedostosta.
    * onko tiedosto olemassa
    * montako peliä tiedostossa on

--- src/net/jokinagames/Main.java
+++ src/net/jokinagames/Main.java
@@ -1,15 +1,24 @@
 package net.jokinagames;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class Main {
 
     public static void main(String[] args) {
 
         // Nathan PGN testin alku - voi kommentoida pois jos ei sitä vielä kaipaa.
 
-        PortableGameNotationReader pgnReader = new PortableGameNotationReader("data\\test_regular_game.pgn");
-        Peli peli = pgnReader.parsePgn();
-        if(peli.peliOhi()) {
-            // ei vielä mitään.
+        try {
+            PortableGameNotationReader pgnReader = new PortableGameNotationReader("data\\test_regular_game.pgn");
+            Peli peli = pgnReader.parsePgn();
+            if (peli.peliOhi()) {
+                // ei vielä mitään.
+            }
+        } catch (FileNotFoundException fnfe) {
+            System.out.println(fnfe.getMessage());
+        } catch (IOException ioe) {
+            System.out.println(ioe.getMessage());
         }
     }
 }
--- src/net/jokinagames/PortableGameNotationReader.java
+++ src/net/jokinagames/PortableGameNotationReader.java
@@ -1,12 +1,14 @@
 package net.jokinagames;
 
+import java.io.*;
+
 /**
  * Luokka, jolla parsitaan PGN-muodon tiedostoja. Luokka osaa
  * antaa Peli-olion jokaista tiedostossa esiintyvää peliä kohden.
- *
+ * <p>
  * PGN-tiedosto voi sisältää FEN-notaatiolla alkuasetelmia perusnappuloilla
  * ja peruslaudalle (8x8).
- *
+ * <p>
  * PGN-spesifikaatio löytyy osoitteesta
  * http://www.saremba.de/chessgml/standards/pgn/pgn-complete.htm
  */
@@ -18,19 +20,43 @@ public class PortableGameNotationReader {
      *
      * @param gameFile polku PGN-tiedostoon.
      */
-    public PortableGameNotationReader(String gameFile) {
+    public PortableGameNotationReader(String gameFile) throws IOException {
+        // TODO tarkista onko tiedosto olemassa
         this.gameFile = gameFile;
+        File f = new File(gameFile);
+        if (!f.isFile()) {
+            throw new FileNotFoundException("Tiedosto ei ole olemassa: " + gameFile + ".");
+        }
+
+        if(laskePelit()==0) {
+            throw new IOException("Tiedostossa ei ole pelejä.");
+        }
     }
 
     /**
      * Parsitaan PGN-tiedostosta ensimmäinen peli.
-     *
+     * <p>
      * Jos PGN-tiedostossa on useampi peli vain ensimmäinen
      * parsitaan.
+     *
      * @return Peli siirtoineen
      */
     public Peli parsePgn() {
         return parsePgn(0);
+    }
+
+    private int laskePelit() {
+        int s = 0;
+        try {
+            BufferedReader br = new BufferedReader(new FileReader(gameFile));
+            String line;
+            while ((line = br.readLine()) != null) {
+                if (line.startsWith("[Event")) s++;
+            }
+        } catch (IOException ioe) {
+
+        }
+        return s;
     }
 
     /**
@@ -40,6 +66,20 @@ public class PortableGameNotationReader {
      * @return Peli-olio valitusta pelistä
      */
     public Peli parsePgn(int index) throws IndexOutOfBoundsException {
+        int pelienMaara = laskePelit();
+        if (index < 0 || index >= pelienMaara) {
+            throw new IndexOutOfBoundsException("Virheellinen indeksi PGN-tiedostoon.");
+        }
+        /*
+            TODO
+            - avaa tiedosto
+            - laske pelien määrä
+            - varmista että index on [0, pelien määrä)
+            - lue PGN-pelin yhteen stringiin
+            - pätkistä tageihin ja siirtomerkkijonoon
+            - parsii pelaajat
+            - parsii muut tagit ja aseta Peliin (tapahtuma, pvm, kierros, jne)
+         */
 
         // TODO parsii pelaajat
         Pelaaja valkoinen = new Pelaaja("eka", Vari.VALKOINEN);
@@ -53,16 +93,18 @@ public class PortableGameNotationReader {
 
         // TODO lue PGN:stä kaikki siirrot ja lisää peliin.
 
+        //
+
         return peli;
     }
 
     /**
      * Parsitaan FEN-notaation mukaisen merkkijonon, ja luodaan
      * sen perusteella Lauta.
-     *
+     * <p>
      * Esimerkki FEN:
      * rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w - - 0 1
-     *
+     * <p>
      * Lisätietoja Wikipediasta osoitteessa
      * https://en.wikipedia.org/wiki/Forsyth%E2%80%93Edwards_Notation
      *
@@ -70,8 +112,8 @@ public class PortableGameNotationReader {
      * @return Lauta
      */
     public Lauta parseFen(String fen) {
-       Lauta fenLauta = new Lauta();
+        Lauta fenLauta = new Lauta();
 
-       return fenLauta;
+        return fenLauta;
     }
 }
