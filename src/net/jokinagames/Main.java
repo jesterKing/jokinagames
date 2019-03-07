ff3b163110a14d0ba34b04f58247d76587b49919
Nathan Letwory <jesterking@letwory.net>
2019-03-07 14:41:02 +0200

    PGN-tiedostojen sijainti. (#1)
    * current working directory, jonka alla data/
    * käytetään File.separator, jotta polut toimivat
      kaikilla tuetuilla alustoilla

--- src/net/jokinagames/Main.java
+++ src/net/jokinagames/Main.java
@@ -1,16 +1,21 @@
 package net.jokinagames;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.nio.file.FileSystems;
 
 public class Main {
 
     public static void main(String[] args) {
+        // haetaan current working dir.
+        // data oletetaan olevan siinä alla, eli $CWD$/data, jne.
+        final String rootFolder = FileSystems.getDefault().getPath(".").normalize().toAbsolutePath().toString();
+        String dataFolder = rootFolder + File.separator + "data" + File.separator;
 
         // Nathan PGN testin alku - voi kommentoida pois jos ei sitä vielä kaipaa.
-
         try {
-            PortableGameNotationReader pgnReader = new PortableGameNotationReader("data\\test_regular_game.pgn");
+            PortableGameNotationReader pgnReader = new PortableGameNotationReader(dataFolder + "test_regular_game.pgn");
             Peli peli = pgnReader.parsePgn();
             if (peli.peliOhi()) {
                 // ei vielä mitään.
