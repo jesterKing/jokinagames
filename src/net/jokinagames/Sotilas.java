7aad82018a1175a0cfd340088e4c14f5c5f62ecf
kimmo <kimmo.hilden@hotmail.com>
2019-03-17 21:05:48 +0200

    Sotilas ja torni, nappulan ja ratsun päivitystä

--- src/net/jokinagames/Sotilas.java
+++ src/net/jokinagames/Sotilas.java
@@ -6,11 +6,52 @@ import java.util.*;
 public class Sotilas extends Nappula {
 
 	public Sotilas(Vari vari) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+		super(vari);
 	}
 
-	public List<Siirto> mahdollisetSiirrot() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	private static final List<int[]> n = Arrays.asList( // Mahdollisten siirtojen luominen
+			new int[]{1, 0},
+			new int[]{1, 1},
+			new int[]{1, -1});
+
+
+	public List<Siirto> mahdollisetSiirrot(Koordinaatti A) {
+		List<Siirto> Siirrot = new ArrayList<>(); // Alustetaan uusi palautettava lista siirroista
+
+		int lr = A.annaRivi(); // lahtorivi
+		int ls = A.annaSarake(); // lahtosarake
+		int kerroin = 0;
+
+		if (this.annaVari() == Vari.VALKOINEN){ // Apukerroin suunnan määrittämiseen, VALKOINEN 1, MUSTA -1
+			kerroin = kerroin +1;
+		} else {
+			kerroin = kerroin -1;
+		}
+
+		for (int[] a:n) { // Käydään mahdollisten siirtojen lista läpi
+			int ur = kerroin * a[0] + lr; // Muuttuja uudelle riville, väri huomioitu kertoimella
+			int us = a[1] + ls; // Muuttuja uudelle sarakkeelle
+			if (ur >= 1 && ur <= 8 && us >= 1 && us <= 8) {  // Tarkistetaan, että ollaanko laudalla ko. siirron tapauksessa
+				Koordinaatti uk = new Koordinaatti(koordinaatit[0].charAt(ur) + "" + koordinaatit[1].charAt(us)); // Luodaan uusi koordinaatti
+				Siirto uS = new Siirto(A, uk);  // Jos ollaan, generoidaan uusi siirto
+				Siirrot.add(uS);                // ja lisätään siirtolistaan
+			}
+		}
+
+		if(lr==1 && this.annaVari() == Vari.VALKOINEN) { // Tarkistetaan vielä oliko ensimmäine siirto vai ei
+			Koordinaatti uK = new Koordinaatti(koordinaatit[0].charAt(lr + 2) + "" + koordinaatit[1].charAt(ls));
+			Siirto uS1 = new Siirto(A, uK);
+			Siirrot.add(uS1);
+		} else if (lr==7 && this.annaVari() == Vari.MUSTA){
+			Koordinaatti uK = new Koordinaatti(koordinaatit[0].charAt(lr - 2) + "" + koordinaatit[1].charAt(ls));
+			Siirto uS1 = new Siirto(A, uK);
+			Siirrot.add(uS1);
+		}
+
+		return Siirrot;
+
 	}
+
 
 }
+
--- src/net/jokinagames/Torni.java
+++ src/net/jokinagames/Torni.java
@@ -6,11 +6,34 @@ import java.util.*;
 public class Torni extends Nappula {
 
 	public Torni(Vari vari) {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+
+		super(vari);
 	}
 
-	public List<Siirto> mahdollisetSiirrot() {
-		throw new UnsupportedOperationException("The method is not implemented yet.");
+	private static final List<int[]> n = Arrays.asList( // Mahdollisten siirtojen luominen
+			new int[]{1, 0},
+			new int[]{-1, 0},
+			new int[]{0, 1},
+			new int[]{0, -1});
+
+	public List<Siirto> mahdollisetSiirrot(Koordinaatti A) {
+		List<Siirto> Siirrot = new ArrayList<>(); // Alustetaan uusi palautettava lista siirroista
+
+		int lr = A.annaRivi(); // lahtorivi
+		int ls = A.annaSarake(); // lahtosarake
+
+		for (int[] a:n){
+			int ur = a[0] + lr; // Muuttuja uudelle riville
+			int us = a[1] + ls; // Muuttuja uudelle sarakkeelle
+			while (ur>=1 && ur<=8 && us>=1 && us<=8){
+				Koordinaatti uk = new Koordinaatti(koordinaatit[0].charAt(ur-1) +""+koordinaatit[1].charAt(us-1)); // Luodaan uusi koordinaatti
+				Siirto uS = new Siirto(A, uk);  // Jos ollaan, generoidaan uusi siirto
+				Siirrot.add(uS);
+				ur = a[0] + ur;
+				us = a[1] + us;
+			}
+		}
+		return Siirrot;
 	}
 
 }
