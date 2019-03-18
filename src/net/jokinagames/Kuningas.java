688e924ad631a1d3308d9ce5ad848ab43d354128
kimmo <kimmo.hilden@hotmail.com>
2019-03-18 19:00:08 +0200

    Kuninkaalle konstruktori ja mahdollisetSiirrot

--- src/net/jokinagames/Kuningas.java
+++ src/net/jokinagames/Kuningas.java
@@ -6,14 +6,38 @@ import java.util.*;
 public class Kuningas extends Nappula {
 
 	public Kuningas(Vari vari) {
-	    // TODO toteuta
+		super(vari);
 	}
+
+	private static final List<int[]> n = Arrays.asList( // Mahdollisten siirtojen luominen
+			new int[]{1, 1},
+			new int[]{1, -1},
+			new int[]{-1, -1},
+			new int[]{-1, 1},
+			new int[]{-1, 0},
+			new int[]{1, 0},
+			new int[]{0, 1},
+			new int[]{0, -1});
 
 	public List<Siirto> mahdollisetSiirrot(Koordinaatti A) {
-		ArrayList<Siirto> siirrot = new ArrayList<>();
-		// TODO toteuta
+		List<Siirto> Siirrot = new ArrayList<>(); // Alustetaan uusi palautettava lista siirroista
 
-		return siirrot;
+		int lr = A.annaRivi(); // lahtorivi
+		int ls = A.annaSarake(); // lahtosarake
+
+		for (int[] a:n) { // Käydään mahdollisten siirtojen lista läpi
+			int ur = a[0] + lr; // Muuttuja uudelle riville, väri huomioitu kertoimella
+			int us = a[1] + ls; // Muuttuja uudelle sarakkeelle
+			if (ur >= 1 && ur <= 8 && us >= 1 && us <= 8) {  // Tarkistetaan, että ollaanko laudalla ko. siirron tapauksessa
+				Koordinaatti uk = new Koordinaatti(koordinaatit[0].charAt(ur) + "" + koordinaatit[1].charAt(us)); // Luodaan uusi koordinaatti
+				Siirto uS = new Siirto(A, uk);  // Jos ollaan, generoidaan uusi siirto
+				Siirrot.add(uS);                // ja lisätään siirtolistaan
+			}
+		}
+
+		return Siirrot;
+
 	}
+
 
 }
