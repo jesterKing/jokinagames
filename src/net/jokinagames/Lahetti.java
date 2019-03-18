9f72ca3413cd4cfdd0bfa29e34bc233568a4808f
kimmo <kimmo.hilden@hotmail.com>
2019-03-18 19:00:54 +0200

    Lähetille konstruktori ja mahdollisetSiirrot

--- src/net/jokinagames/Lahetti.java
+++ src/net/jokinagames/Lahetti.java
@@ -6,14 +6,33 @@ import java.util.*;
 public class Lahetti extends Nappula {
 
 	public Lahetti(Vari vari) {
-	    // TODO toteuta
+			super(vari);
+		}
+
+		private static final List<int[]> n = Arrays.asList( // Mahdollisten siirtojen luominen
+				new int[]{1, 1},
+				new int[]{1, -1},
+				new int[]{-1, -1},
+				new int[]{-1, 1});
+
+		public List<Siirto> mahdollisetSiirrot(Koordinaatti A) {
+			List<Siirto> Siirrot = new ArrayList<>(); // Alustetaan uusi palautettava lista siirroista
+
+			int lr = A.annaRivi(); // lahtorivi
+			int ls = A.annaSarake(); // lahtosarake
+
+			for (int[] a:n){
+				int ur = a[0] + lr; // Muuttuja uudelle riville
+				int us = a[1] + ls; // Muuttuja uudelle sarakkeelle
+				while (ur>=1 && ur<=8 && us>=1 && us<=8){
+					Koordinaatti uk = new Koordinaatti(koordinaatit[0].charAt(ur-1) +""+koordinaatit[1].charAt(us-1)); // Luodaan uusi koordinaatti
+					Siirto uS = new Siirto(A, uk);  // Jos ollaan, generoidaan uusi siirto
+					Siirrot.add(uS);
+					ur = a[0] + ur;
+					us = a[1] + us;
+				}
+			}
+			return Siirrot;
+		}
+
 	}
-
-	public List<Siirto> mahdollisetSiirrot(Koordinaatti A) {
-		ArrayList<Siirto> siirrot = new ArrayList<>();
-	    // TODO toteuta
-
-		return siirrot;
-	}
-
-}
