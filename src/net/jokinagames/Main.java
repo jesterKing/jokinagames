c91c18f2f0aa413c780b0d09ae4a621d21d90e51
Nathan Letwory <nathan@mcneel.com>
2019-03-26 06:49:41 +0200

    Nimetään muuttujia luettavammiksi.

--- src/net/jokinagames/Main.java
+++ src/net/jokinagames/Main.java
@@ -1,9 +1,5 @@
 package net.jokinagames;
 
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.nio.file.FileSystems;
 import java.util.Scanner;
 
 public class Main {
@@ -26,46 +22,46 @@ public class Main {
             System.out.println(fnfe.getMessage());
         }
         */
-        Scanner s = new Scanner(System.in);             //Uus skanneri syötteen lukemiseen.
+        Scanner skanneri = new Scanner(System.in);             //Uus skanneri syötteen lukemiseen.
         System.out.println("Tervetuloa pelaamaan shakkia!");
         System.out.println("Syötä 1 jos haluat uuden pelin.");
         System.out.println("Syötä 2 jos haluat jatkaa vanhaa peliä.");
-        int valinta = s.nextInt();
+        int valinta = skanneri.nextInt();
         if(valinta==1) {
             System.out.println("Haluatko tavallisen vai transcendental aloituksen?");
             System.out.println("Syötä 1 tavalliseen aloitukseen.");
             System.out.println("Syötä 2 transcendental aloitukseen.");
-            int peliasetelma = s.nextInt();
+            int peliasetelma = skanneri.nextInt();
             if (peliasetelma == 1) {
                 System.out.println("Anna pelaajan 1 nimi (Valkoiset).");
-                s.nextLine();
-                String nimi = s.nextLine();
-                Pelaaja yy = new Pelaaja(nimi, Vari.VALKOINEN);
+                skanneri.nextLine();
+                String nimi = skanneri.nextLine();
+                Pelaaja valkoinenPelaaja = new Pelaaja(nimi, Vari.VALKOINEN);
                 System.out.println("Anna pelaajan 2 nimi (Mustat)");
-                nimi = s.nextLine();
-                Pelaaja kaa = new Pelaaja(nimi, Vari.MUSTA);
-                Lauta uus = PortableGameNotationReader.alustaTavallinenPeli();
-                Peli uuspeli = Peli.uusiPeli(yy, kaa, uus);
+                nimi = skanneri.nextLine();
+                Pelaaja mustaPelaaja = new Pelaaja(nimi, Vari.MUSTA);
+                Lauta alkuLauta = PortableGameNotationReader.alustaTavallinenPeli();
+                Peli uuspeli = Peli.uusiPeli(valkoinenPelaaja, mustaPelaaja, alkuLauta);
                 int siirtovuoro = 0;
                 while (!uuspeli.peliOhi()) {
-                    handlaaVuoro(siirtovuoro%2==0?yy:kaa,uuspeli, s);
+                    handlaaVuoro(siirtovuoro%2==0?valkoinenPelaaja:mustaPelaaja,uuspeli, skanneri);
                     siirtovuoro++;
                 }
                 System.out.println("Peli ohi! ");
             }
             if (peliasetelma == 2) {
                 System.out.println("Anna pelaajan 1 nimi (Valkoiset).");
-                s.nextLine();
-                String nimi1 = s.nextLine();
-                Pelaaja yy = new Pelaaja(nimi1, Vari.VALKOINEN);
+                skanneri.nextLine();
+                String nimi1 = skanneri.nextLine();
+                Pelaaja valkoinenPelaaja = new Pelaaja(nimi1, Vari.VALKOINEN);
                 System.out.println("Anna pelaajan 2 nimi (Mustat)");
-                String nimi2 = s.nextLine();
-                Pelaaja kaa = new Pelaaja(nimi2, Vari.MUSTA);
-                Lauta uus = PortableGameNotationReader.alustaTranscendentalPeli();
-                Peli uuspeli = Peli.uusiPeli(yy, kaa, uus);
+                String nimi2 = skanneri.nextLine();
+                Pelaaja mustaPelaaja = new Pelaaja(nimi2, Vari.MUSTA);
+                Lauta alkuLauta = PortableGameNotationReader.alustaTranscendentalPeli();
+                Peli uuspeli = Peli.uusiPeli(valkoinenPelaaja, mustaPelaaja, alkuLauta);
                 int siirtovuoro = 0;
                 while (!uuspeli.peliOhi()) {
-                    handlaaVuoro(siirtovuoro%2==0?yy:kaa,uuspeli, s);
+                    handlaaVuoro(siirtovuoro%2==0?valkoinenPelaaja:mustaPelaaja,uuspeli, skanneri);
                     siirtovuoro++;
                 }
                 System.out.println("Peli ohi! ");
