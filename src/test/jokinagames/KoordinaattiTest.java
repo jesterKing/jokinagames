0d9ef77b4300a2160b2fd10b59bc8a014e01a39c
Nathan Letwory <nathan@mcneel.com>
2019-03-27 14:39:12 +0200

    Alustava JUnit 4.12:n käyttöönotto.

---
+++ src/test/jokinagames/KoordinaattiTest.java
@@ -0,0 +1,34 @@
+package test.jokinagames;
+
+import net.jokinagames.Koordinaatti;
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.hamcrest.CoreMatchers.is;
+
+public class KoordinaattiTest {
+
+    @Test
+    public void annaSarake() {
+        Koordinaatti a1 = new Koordinaatti("a1");
+        Assert.assertThat(a1.annaSarake(), is(0));
+        Koordinaatti h1 = new Koordinaatti("h1");
+        Assert.assertThat(h1.annaSarake(), is(7));
+    }
+
+    @Test
+    public void annaRivi() {
+        Koordinaatti a1 = new Koordinaatti("a1");
+        Assert.assertThat(a1.annaRivi(), is(0));
+        Koordinaatti h1 = new Koordinaatti("h1");
+        Assert.assertThat(h1.annaRivi(), is(0));
+    }
+
+    @Test
+    public void annaSan() {
+        Koordinaatti a1 = new Koordinaatti("a1");
+        Assert.assertThat(a1.annaSan(), is("a1"));
+        Koordinaatti h1 = new Koordinaatti("h1");
+        Assert.assertThat(h1.annaSan(), is("h1"));
+    }
+}
